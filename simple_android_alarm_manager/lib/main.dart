c276d878b6826724738d40279b119d1bd5c89f72
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-03-04 20:17:54 +0700

    learn alarm manager in background task

--- simple_android_alarm_manager/lib/main.dart
+++ simple_android_alarm_manager/lib/main.dart
@@ -1,20 +1,26 @@
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_android_alarm_manager/ui/home_page.dart';
+import 'package:simple_android_alarm_manager/utils/background_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final BackgroundService service = BackgroundService();
+  service.initializeIsolate();
+  AndroidAlarmManager.initialize;
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
+  static const title = "Simple Alarm Manager ";
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title: title,
+      home: HomePage(title: title),
     );
   }
 }
